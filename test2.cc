4e7f2c1e8eadf8a95f9d30fece5416cf25c7ec20
haohb13 <haohb13@gmail.com>
2020-10-23 17:51:45 +0800

    lwh:add tet2.cc file

---
+++ test2.cc
@@ -0,0 +1,15 @@
+ ///
+ /// @file    test2.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2020-10-23 17:51:25
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+ 
+int main(void)
+{
+	test0();
+	return 0;
+}
