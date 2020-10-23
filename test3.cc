2c3223aa8a7701fbc0a46eb8f6d03bc6a7e2009c
haohb13 <haohb13@gmail.com>
2020-10-23 17:52:37 +0800

    lwh:add test3.cc file

---
+++ test3.cc
@@ -0,0 +1,15 @@
+ ///
+ /// @file    test3.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2020-10-23 17:52:16
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+ 
+int main(void)
+{
+	test0();
+	return 0;
+}
