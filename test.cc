12bef91340fdf233df0cea3e55f486e06e2fbf8a
haohb13 <haohb13@gmail.com>
2020-10-23 16:49:53 +0800

    lwh:test

---
+++ test.cc
@@ -0,0 +1,15 @@
+ ///
+ /// @file    test.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2020-10-23 16:49:20
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+ 
+int main(void)
+{
+	cout << "hello,world!" << endl;
+	return 0;
+}
